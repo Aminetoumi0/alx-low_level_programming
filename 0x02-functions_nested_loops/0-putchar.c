0d3efe368ab219faa4daff48e81917fe7b7b49e2
Aminetoumi0 <toumibusiness0@gmail.com>
2023-03-21 05:24:47 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
 #include <main.h>
 /*
- * main - Program that prints putchar
- *
- * Return: Always 0
+ * main -  print _putchar
+ * is written by mbah
+ * Return: 0
  */
 int main(void)
 {
-	char="_putchar";
-
-	_putchar(char);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
