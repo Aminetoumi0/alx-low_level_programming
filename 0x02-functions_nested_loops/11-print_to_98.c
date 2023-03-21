1208251d17c7ec875be98e34bd393a81bf9658bd
Aminetoumi0 <toumibusiness0@gmail.com>
2023-03-21 09:22:51 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,8 +7,32 @@
 void print_to_98(int n)
 {
 	int i, j;
-	for 
+
+	if (n <= 98)
 	{
-		_putchar(n);
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d", i);
+			}
+			else if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+			{
+				printf("%d", j);
+			}
+			else if (j == 98)
+			{
+				printf("%d\n", j);
+			}
+		}
+	}
+}
