83be32ceb5459b83acf5adfdbdccc72aedbab2cc
Aminetoumi0 <toumibusiness0@gmail.com>
2023-03-23 06:03:53 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers
+ * print_numbers - prints numbers 0 - 9
+ *
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int i;
+	char i;
 
 	for (i = '0'; i < '10'; i++)
 	{
